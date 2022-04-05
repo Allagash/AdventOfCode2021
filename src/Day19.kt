85b308e6b166cdfb3c8dc67005ea781f08c0f573
Jeff Somers <somersj@amazon.com>
2022-04-05 14:40:50 -0400

    Day19 working

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,90 +1,129 @@
 // Advent of Code 2021, Day 19, Beacon Scanner
 
+import kotlin.math.abs
+import kotlin.math.max
+
+// https://github.com/xorum-io/advent-of-code-2021/blob/main/src/Day19.kt
+// from https://raw.githubusercontent.com/Jbrusaw/AdventOfCode2021/main/src/day19.kt
 
 fun main() {
 
-    // sort by x, then store the deltas between x values. Do the same for y, etc.
+    data class Point(val x: Int, val y: Int, val z: Int) {
+        operator fun plus(other: Point): Point {
+            return Point(x + other.x, y + other.y, z + other.z)
+        }
 
-    data class Scanner(val readings: Set<List<Int>>, val gaps: List<List<Int>>? = null) {
-
-        // these are wrong
-        // use https://www.wolframalpha.com/input/?i=yaw%3D180+degrees%2C+pitch%3D90+degrees
-        val indexPerms = listOf(
-            listOf(0, 1, 2), listOf(0, 2, 1),
-            listOf(1, 0, 2), listOf(1, 2, 0),
-            listOf(2, 0, 1), listOf(2, 1, 0)
-        )
-
-        val rotates = listOf(
-            // no mirror images (can't have odd number of -1 per list)
-            listOf(1, 1, 1), listOf(1, -1, -1),
-            listOf(-1, 1, -1), listOf(-1, -1, 1),
-        )
-
-        val scannerTransforms = mutableListOf<Scanner>() // 24 in size, see problem description
-
-        fun populateTransforms() {
-            indexPerms.forEach { p ->
-                rotates.forEach { r ->
-                    val transformed = mutableSetOf<List<Int>>()
-                    readings.forEach {
-                        val newCoord = it.mapIndexed { idx, _ -> it[p[idx]] * r[idx] }
-                        transformed.add(newCoord)
-                    }
-                    // sort by x, save gaps
-                    //orrr, map x -> each coord, sorted by x
-                    // for now , just generate gap fingerprint
-                    val gaps = mutableListOf<List<Int>>()
-                    for (i in 0..2) {
-                        val sortedByOneAxis = transformed.sortedBy { it[0] }.map { it[0] }
-                        val diffs = mutableListOf<Int>()
-                        for (idx in 0..sortedByOneAxis.size - 2) { // is this right last index?
-                            diffs.add(sortedByOneAxis[idx + 1] - sortedByOneAxis[idx])
-                        }
-                        gaps.add(diffs)
-                    }
-                    scannerTransforms.add(Scanner(transformed, gaps))
-                }
-            }
+        operator fun minus(other: Point): Point {
+            return Point(x - other.x, y - other.y, z - other.z)
         }
     }
 
+    // sort by x, then store the deltas between x values. Do the same for y, etc.
 
-    fun part1(input: List<Scanner>): Long {
-        input.forEach {
-            it.populateTransforms()
+    data class Scanner(val id: Int, var readings: List<Point>) {
+
+        var done = false
+        var origin = Point(0, 0, 0)
+
+        fun rotate(i: Int) : List<Point> {
+            check(i in 0..23)
+            return when (i) {
+                // from https://www.wolframalpha.com/input/?i=yaw%3D180+degrees%2C+pitch%3D90+degrees
+                0 ->  readings.map{ Point( it.x,  it.y,  it.z)} // same thing
+                1 ->  readings.map{ Point( it.x,  it.z, -it.y)} // roll 90
+                2 ->  readings.map{ Point( it.x, -it.y, -it.z)} // roll 180
+                3 ->  readings.map{ Point( it.x, -it.z,  it.y)} // roll 270
+
+                4 ->  readings.map{ Point( it.y, -it.x,  it.z)} // yaw 90
+                5 ->  readings.map{ Point( it.z, -it.x, -it.y)} // yaw 90 roll 90
+                6 ->  readings.map{ Point(-it.y, -it.x, -it.z)}
+                7 ->  readings.map{ Point(-it.z, -it.x,  it.y)}
+
+                8 ->  readings.map{ Point(-it.x, -it.y,  it.z)} // yaw 180, roll 0
+                9 ->  readings.map{ Point(-it.x, -it.z, -it.y)}
+                10 -> readings.map{ Point(-it.x,  it.y, -it.z)}
+                11 -> readings.map{ Point(-it.x,  it.z,  it.y)}
+
+                12 -> readings.map{ Point(-it.y,  it.x,  it.z)} // yaw 270, pitch 0
+                13 -> readings.map{ Point(-it.z,  it.x, -it.y)}
+                14 -> readings.map{ Point( it.y,  it.x, -it.z)}
+                15 -> readings.map{ Point( it.z,  it.x,  it.y)}
+
+                16 -> readings.map{ Point( it.z,  it.y, -it.x)} // pitch 90, roll 0
+                17 -> readings.map{ Point(-it.y,  it.z, -it.x)} // pitch 90, roll 90
+                18 -> readings.map{ Point(-it.z, -it.y, -it.x)}
+                19 -> readings.map{ Point( it.y, -it.z, -it.x)}
+
+                20 -> readings.map{ Point(-it.z,  it.y,  it.x)} // pitch -90, roll 0
+                21 -> readings.map{ Point( it.y,  it.z,  it.x)} // pitch -90, roll 90
+                22 -> readings.map{ Point( it.z, -it.y,  it.x)}
+                23 -> readings.map{ Point(-it.y, -it.z,  it.x)}
+                else -> throw Exception("$i out of range for rotate")
+            }
         }
-        // for  scanner 0
-        val scanner0 = input[0]
-        for (i in 1 until input.size) {
-            input[i].scannerTransforms.forEachIndexed { idx, it ->
-                for (x in -1000..1000) {
-                    println("input $i, x = $x")
-                    for (y in -1000..1000) {
-                        for (z in -1000..1000) {
-                            val moveAmount = listOf(x, y, z)
-                            // move all points
-                            val setPts = mutableSetOf<List<Int>>()
-                            it.readings.forEachIndexed { jidx, r ->
-                                setPts.add(r.mapIndexed { midx, it -> it + moveAmount[midx] })
-                            }
-                            // what is best way to do set intersection? or don't use sets?
-                            val size = scanner0.readings.filter { it in setPts }.size
-                            if (size > 0) {
-                                println("scanner $i, transform $idx, add ($x, $y, $z), intersects $size")
-                            }
-                            // intersect with scanner0?
 
+        fun rotateAndCheck(scanner: Scanner) : Boolean {
+            if (scanner.done) {
+                return false
+            }
+            for(i in 0..23) {
+                val rotatedPts = scanner.rotate(i)
+                rotatedPts.forEach { r ->
+                    readings.forEach { orig ->
+                        val newOrigin = orig - r
+                        val count = rotatedPts.map { it + newOrigin }.count{ readings.contains(it)}
+                        if (count >= 12) {
+                            scanner.done = true
+                            scanner.readings = rotatedPts.map { it + newOrigin }
+                            scanner.origin = newOrigin
+                            return true
                         }
                     }
                 }
             }
+            return false
         }
-        // for each other scanner
-        // transform each scanner by -1000..1000 in each direction
-        // look for set intersection
+    }
 
-        return 0L
+    fun manhattanDistance(scanner1 : Scanner, scanner2: Scanner) : Int {
+        val pt = scanner1.origin - scanner2.origin
+        return abs(pt.x) + abs(pt.y) + abs(pt.z)
+    }
+
+
+    fun part1(input: List<Scanner>): Int {
+        val located = mutableListOf<Scanner>()
+        val todo = input.toMutableList()
+        located.add(input[0])
+        todo.remove(input[0])
+
+        val done = mutableListOf<Scanner>()
+
+        while(todo.isNotEmpty()) {
+            located.forEach { l ->
+                todo.forEach { t ->
+                    if (l.rotateAndCheck(t)) {
+                        done.add(t)
+                    }
+                }
+            }
+            check(done.isNotEmpty())
+            located.addAll(done)
+            todo.removeAll(done)
+            done.clear()
+        }
+        var maxDistance = 0
+        located.forEachIndexed { i, it ->
+            for (j in i+1 .. located.lastIndex) {
+                maxDistance = max(maxDistance, manhattanDistance(it, located[j]))
+            }
+        }
+
+        println("dist = $maxDistance")
+
+
+        val uniqueBeacons = located.map {it.readings}.flatten().distinct().size
+        return uniqueBeacons
     }
 
     fun part2(input: List<Scanner>): Long {
@@ -92,30 +131,35 @@ fun main() {
     }
 
     fun parse(input: List<String>): List<Scanner> {
-
         val scanners = mutableListOf<Scanner>()
-        val setPts = mutableSetOf<List<Int>>()
+        val pts = mutableListOf<Point>()
+        var id = -1
         input.forEach {
-            if (it.trim().isEmpty() || it.contains("--- scanner ")) {
-                if (setPts.isNotEmpty()) {
-                    scanners.add(Scanner(setPts.toSet())) // deep copy of set of points
-                    setPts.clear()
+            val beginScanner = it.contains("--- scanner ")
+            if (it.trim().isEmpty() || beginScanner) {
+                if (beginScanner) {
+                    id = it.split("--- scanner ")[1].split(" ---")[0].toInt()
+                }
+                if (pts.isNotEmpty()) {
+                    scanners.add(Scanner(id, pts.toList())) // use toList() to get deep copy list of points
+                    pts.clear()
                 }
                 return@forEach // like a continue
             }
-            setPts.add(it.split(',').map { it.toInt() })
+            val (x, y, z) = it.trim().split(',').map { it.toInt() }
+            pts.add(Point(x, y, z))
         }
-        if (setPts.isNotEmpty()) {
-            scanners.add(Scanner(setPts.toSet())) // deep copy of set of points
+        if (pts.isNotEmpty()) {
+            scanners.add(Scanner(id, pts.toList())) // use toList() to get deep copy list of points
         }
         return scanners
     }
 
     val testInput = parse(readInput("Day19_test"))
-    check(part1(testInput) == 0L)
-    check(part2(testInput) == 0L)
+    check(part1(testInput) == 79)
+   // check(part2(testInput) == 0L)
 
-    val input = readInput("Day19")
-//    println(part1(input))
+    val input = parse(readInput("Day19"))
+    println(part1(input))
 //    println(part2(input))
 }
