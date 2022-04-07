ddd8b41ae5b146f596bc2a943d6de01de8e947a4
Jeff Somers <somersj@amazon.com>
2022-04-06 21:29:04 -0400

    Day 22 part 2 working

--- src/Day22.kt
+++ src/Day22.kt
@@ -4,11 +4,24 @@ import kotlin.math.min
 
 fun main() {
 
-    data class Cuboid(val on: Boolean,
-                      val minx: Int, val maxx: Int,
-    val miny: Int, val maxy: Int,
-    val minz: Int, val maxz: Int
-    )
+    data class Cuboid(
+        val on: Boolean,
+        val minx: Long, val maxx: Long,
+        val miny: Long, val maxy: Long,
+        val minz: Long, val maxz: Long
+    ) {
+        fun intersection(other: Cuboid) : Cuboid? {
+            return if (maxx >= other.minx && maxy >= other.miny && maxz >= other.minz &&
+                       minx <= other.maxx && miny <= other.maxy && minz <= other.maxz) {
+                Cuboid(!other.on,
+                    max(minx, other.minx), min(maxx, other.maxx),
+                    max(miny, other.miny), min(maxy, other.maxy),
+                    max(minz, other.minz), min(maxz, other.maxz))
+            } else {
+                null
+            }
+        }
+    }
 
     fun parse(input: List<String>, part1: Boolean): List<Cuboid> {
         val cubes = mutableListOf<Cuboid>()
@@ -17,9 +30,9 @@ fun main() {
             val vals = it.split(" x=", "..", ",y=", ",z=")
             val isOn = vals[0] == "on"
             cubes.add(Cuboid(
-                isOn, vals[1].toInt(), vals[2].toInt(),
-                vals[3].toInt(), vals[4].toInt(),
-                vals[5].toInt(), vals[6].toInt()
+                isOn, vals[1].toLong(), vals[2].toLong(),
+                vals[3].toLong(), vals[4].toLong(),
+                vals[5].toLong(), vals[6].toLong()
             ))
         }
         if (!part1) {
@@ -33,32 +46,11 @@ fun main() {
             max(it.miny, -50), min(it.maxy, 50),
             max(it.minz, -50), min(it.maxz, 50)))
         }
-        // set min, max 50?
         return cubes2
     }
 
     fun part1(cuboids: List<Cuboid>): Int {
-        val pts = mutableSetOf<Triple<Int, Int, Int>>()
-        cuboids.forEach {
-            for (x in it.minx..it.maxx) {
-                for (y in it.miny..it.maxy) {
-                    for (z in it.minz..it.maxz) {
-                        if (it.on) {
-                            pts.add(Triple(x, y, z))
-                        } else {
-                            pts.remove(Triple(x, y, z))
-                        }
-                    }
-                }
-            }
-        }
-
-        return pts.size
-    }
-
-    fun part2(cuboids: List<Cuboid>): Int {
-        println("part2, size: ${cuboids.size}")
-        val pts = mutableSetOf<Triple<Int, Int, Int>>()
+        val pts = mutableSetOf<Triple<Long, Long, Long>>()
         cuboids.forEach {
             for (x in it.minx..it.maxx) {
                 for (y in it.miny..it.maxy) {
@@ -75,17 +67,38 @@ fun main() {
         return pts.size
     }
 
-    val testInput = parse(readInput("Day22_testb"), true)
-    check(part1(testInput) == 590784)
+    // See https://github.com/xorum-io/advent-of-code-2021/blob/main/src/Day22.kt
+    fun part2(cuboids: List<Cuboid>): Long {
+        val stack = mutableListOf<Cuboid>()
+        cuboids.forEach { cit->
+            val newOperations = mutableListOf<Cuboid>()
+            stack.forEach { jit->
+                cit.intersection(jit)?.let { newOperations.add(it) }
+            }
+            stack.addAll(newOperations)
+            // Only apply if turning cubes on. For instance, if
+            // all cubes are off & we have an off instruction, we don't
+            // do anything. We only care about off instructions if they
+            // intersect previous instructions.
+            if (cit.on) {
+                stack.add(cit)
+            }
+        }
+        return stack.sumOf {
+            (if (it.on) 1 else -1) *
+                    (it.maxx - it.minx + 1) *
+                    (it.maxy - it.miny + 1) *
+                    (it.maxz - it.minz + 1)
+        }
+    }
 
-    val testInputC = parse(readInput("Day22_testc"), false)
-    println(part2(testInputC))
+    var testInput = parse(readInput("Day22_testd"), true)
+    check(part1(testInput) == 474140)
+    testInput = parse(readInput("Day22_testd"), false)
+    check(part2(testInput) == 2758514936282235L)
 
-//    check(imageEnhance(testInput, 2) == 35L)
-//    check(imageEnhance(testInput, 50) == 3351L)
-
-    val input = parse(readInput("Day22"), true)
+    var input = parse(readInput("Day22"), true)
     println(part1(input))
-//    println(imageEnhance(input, 2))
-//    println(imageEnhance(input, 50))
+    input = parse(readInput("Day22"), false)
+    println(part2(input))
 }
