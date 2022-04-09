4beae2d96142c44ce44d8a427e9d8d1566fe23ee
Jeff Somers <somersj@amazon.com>
2022-04-09 07:23:50 -0400

    Crappy version that's too slow even though ALU code replaced with 20 lines of Kotlin

--- src/Day24.kt
+++ src/Day24.kt
@@ -9,21 +9,30 @@ fun main() {
 
     var count = 0L
 
-    fun getNextNum(num: Long) : Pair<Long, MutableList<Long>> {
+    fun MutableList<Int>.setToNextLowerNum() {
+        var lastIdx = lastIndex
+        for (i in lastIndex downTo 0) {
+            lastIdx = i
+            if (this[lastIdx] != 1) break
+        }
+        this[lastIdx] -= 1
+        for (i in lastIdx+1.. lastIndex) {
+            this[i] = 9
+        }
+    }
+
+    fun getNextNum(num: Long) : Pair<Long, MutableList<Int>> {
         var num1 = num
         var num2 = num
-        val digits = mutableListOf<Long>()
+        val digits = mutableListOf<Int>()
 
         while (num1 > 0) {
-            val digit = num1 % 10L
-            if (digit == 0L) {
+            val digit = (num1 % 10L).toInt()
+            if (digit == 0) {
                 num2--
                 num1 = num2
                 count++
                 digits.clear()
-                if (count % 100000L == 0L) {
-                    println("num = $num1")
-                }
                 continue
             }
             digits.add(0, digit)
@@ -32,49 +41,101 @@ fun main() {
         return Pair(num2, digits)
     }
 
+    fun getDigits(num: Long) : MutableList<Int> {
+        check(num > 0)
+        val digits = emptyList<Int>().toMutableList()
+        var mynum = num
+        while (mynum > 0) {
+            digits.add((mynum % 10).toInt())
+            mynum /= 10
+        }
+        return digits.asReversed()
+    }
+
+    fun getZ(digits: List<Int>) : Long {
+        var z = digits[0] + 4L
+        z = z * 26 + (digits[1]  + 16)
+        z = (z * 26) + digits[2] + 14
+        val check34 = (digits[3] == digits[2] + 1)
+        z = (z / 26) * (if (check34) 1 else 26) + (digits[3] + 3) * (if (check34) 0 else 1)
+        z = z * 26 + digits[4] + 11
+        z = z * 26 + digits[5] + 13
+        val check7 =  (z % 26 - 7) == digits[6].toLong()
+        z = (z/26) * ( if (check7) 1 else 26) + (if (check7) 0 else (digits[6] + 11))
+        z = z * 26 + digits[7] + 7
+        val check9 = ( z % 26 - 12) == digits[8].toLong()
+        z = (z / 26 * (25 * ( if (check9) 0 else 1) + 1)) +  (digits[8]+ 12) *  ( if (check9) 0 else 1)
+        z = z * 26 +  digits[9]+ 15
+        val check11 = ((z % 26 - 16) == digits[10].toLong())
+        z = z / 26 * (25 * ( if (check11) 0 else 1) + 1) + (digits[10] + 13) * ( if (check11) 0 else 1)
+        val check12 = (z % 26 -9) == digits[11].toLong()
+        z = ( z/26 * ( 25 * (if (check12) 0 else 1) + 1)) +  (digits[11] + 1) *  if (check12) 0 else 1
+        val check13 = (z % 26 - 8) == digits[12].toLong()
+        z = z/26 * ( if (check13) 1 else 26) + if (check13) 0 else (digits[12]+ 15)
+        val check14 = ( z % 26 - 8) == digits[13].toLong()
+        z = z / 26 * ( if (check14) 1 else 26) + (if (check14) 0 else (digits[13] + 4))
+        return z
+    }
+
     fun part1(input: List<String>): Long {
-        var done = false
-        var num = 98765432198765L
+        var minZ = Long.MAX_VALUE
+
+        var count = 0
+
+        var test  = emptyList<Int>().toMutableList()
+        for (i in 1..3) test.add(9)
+        println("test = $test")
         do {
+            test.setToNextLowerNum()
+            println("test = $test")
+        } while (!test.all{it == 1})
+
+        for (num in 99999999999999L downTo 10000000000000L) {
             // check that no digit is 0!
-            val (newNum, digits) = getNextNum(num)
-            num = newNum
+            // val (newNum, digits) = getNextNum(num)
+            // num = newNum
+            val digits = getDigits(num)
+            if (digits.contains(0)) continue
 
-            val register = mutableMapOf<Char, Long>()
-            register['w'] = 0L
-            register['x'] = 0L
-            register['y'] = 0L
-            register['z'] = 0L
+            val register = mutableMapOf('w' to 0L, 'x' to 0L, 'y' to 0L, 'z' to 0L)
 
-            input.forEach {
-                val inst = it.split(' ')
-                val firstReg = inst[1][0]
-                val firstRegVal = register[firstReg]!!
-                when (inst[0]) {
-                    "inp" -> register[firstReg] = digits.removeFirst()
-                    "add" -> register[firstReg] = firstRegVal + getSecondParam(inst, register)
-                    "mul" -> register[firstReg] = firstRegVal * getSecondParam(inst, register)
-                    "div" -> register[firstReg] = firstRegVal / getSecondParam(inst, register)
-                    "mod" -> register[firstReg] = firstRegVal % getSecondParam(inst, register)
-                    "eql" -> register[firstReg] = if (firstRegVal == getSecondParam(inst, register)) 1L else 0L
-                    else -> throw Exception("bad instruction ${inst[0]}")
-                    }
+//            input.forEach {
+//                val inst = it.split(' ')
+//                val firstReg = inst[1][0]
+//                val firstRegVal = register[firstReg]!!
+//                when (inst[0]) {
+//                    "inp" -> register[firstReg] = digits.removeFirst().toLong()
+//                    "add" -> register[firstReg] = firstRegVal + getSecondParam(inst, register)
+//                    "mul" -> register[firstReg] = firstRegVal * getSecondParam(inst, register)
+//                    "div" -> register[firstReg] = firstRegVal / getSecondParam(inst, register)
+//                    "mod" -> register[firstReg] = firstRegVal % getSecondParam(inst, register)
+//                    "eql" -> register[firstReg] = if (firstRegVal == getSecondParam(inst, register)) 1L else 0L
+//                    else -> throw Exception("bad instruction ${inst[0]}")
+//                }
+//            }
+            val newz = getZ(getDigits(num))
+//            check(newz == register['z']!!)
+            //
+            count++
+            if (count > 1000000) {
+                count = 0
+                println("num = $num")
             }
-            if (register['z'] == 0L) {
-                done = true
-                return num
-            } else {
-                num--
+            //println("input: $num, new z = $newz, $register")
+            if (minZ > newz) {
+                //println("input: $num, $register")
+                minZ =  newz
+                println("New min value for $num: $minZ")
+                if (minZ == 0L) return num
             }
-        } while(!done)
-
+        }
         return 0L
     }
 
 
-//    val testInput1 = readInput("Day24_test")
-//    check(part1(testInput1) == 0L)
+    val testInput1 = readInput("Day24_test")
+    part1(testInput1)
 
-    val input = readInput("Day24")
-    println(part1(input))
+//    val input = readInput("Day24")
+//    println(part1(input))
 }
